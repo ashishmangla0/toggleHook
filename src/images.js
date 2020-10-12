aafb30ff65f671178f016715d1fb176612483a00
ashish <ashishmangla1991@gmail.com>
2020-10-12 22:24:11 +0530

    add useEffect

--- src/images.js
+++ src/images.js
@@ -1,14 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import Image1 from './img1.jpg';
 
+// class Image extends Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {intervel:null}
+//     }
+// componentDidMount(){
+//     console.log("image component mounte")
+//     this.setState({
+//         intervel:setInterval(()=>{
+//             console.log(Date());
+//         },1000)
+//     })
+// }
+// componentWillUnmount(){
+//     console.log("image component Unmounte");
+//     clearInterval(this.state.intervel);
+// }
+//     render(){
+//         return(
+//             <img  src={Image1} alt="image 1"/>
+//         )
+//     }
+// }
 
-const Image = () =>{
-useEffect(()=>{
 
-},[])
+const Image = () => {
+    const [datenow, setDateNow] = useState(null);
+    useEffect(() => {
+        
+        console.log("image component mounte");
+        setDateNow(
+            setInterval(() => {
+                console.log(Date());
+            }, 1000)
+        );
+
+        return () => {
+            clearInterval(datenow);
+            console.log('impage unmount');
+            
+        }
+
+    }, [])
+
     return (
         <>
-            <img  src={Image1} alt="image 1"/>
+            {console.log("image component render")}
+            <img src={Image1} alt="image 1" />
         </>
     )
 }
