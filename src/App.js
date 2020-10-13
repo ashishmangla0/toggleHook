d56665c0a223e34e9958556491f81891a97620b9
ashish <ashishmangla1991@gmail.com>
2020-10-13 23:18:59 +0530

    add useref

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Image from './images';
 function App() {
   const [title, setTitle] = useState('Hello React');
   const [isShowing, setIsShowing] = useState(false);
   //const [isShowing,setIsShowing] =  useState(null); //this is jugard 
-  const [update, setUpdate] = useState(false);
+  const [update, setUpdate] = useState(false); //this is jugard2
+  const moutRef =  useRef(false);
   useEffect(() => {
-    setUpdate(true)
+    // setUpdate(true)
     console.log("app mounted");
   }, []);
 
   //this code will help to stop run on every time
+
+
+  //componet didmount
   useEffect(() => {
-    if (update) {
+    if(moutRef.current){
       console.log('app update')
     }
+    else{
+      moutRef.current =  true;
+    }
+    // if (update) {
+    //   console.log('app update')
+    // }
   }, [isShowing])
   const handleClick = () => {
     setIsShowing(!isShowing);
